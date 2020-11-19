c99c9ede963a4136635efaf21e82d95e8065d4d2
tera24d <tera12d@gmail.com>
2020-11-19 16:56:34 +0900

    ActiveHashの実装とアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :prefecture
+  belongs_to :shipping_fee_status
+  belongs_to :scheduled_delivery
 end
