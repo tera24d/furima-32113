aee910f6d3364d8846c3cd0898699f2cb8e1a840
tera24d <tera12d@gmail.com>
2020-11-20 16:00:57 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    #binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:info,:image,:category_id,:sales_status_id,:prefecture_id,:shipping_fee_status_id,:scheduled_delivery_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,21 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_fee_status
   belongs_to :scheduled_delivery
+  belongs_to :user
 
-  validates :name, :info, :price, :image, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :sales_status_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
-  validates :schedule_delivery_id, numericality: { other_than: 1 } 
-
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 8 }
+    validates :info, length: { maximum: 1000 }
+    validates :price, numericality: { greater_than_or_equal_to: 300}
+    validates :price, numericality: { less_than_or_equal_to: 300}
+  
+    with_options numericality: { other_than: 0, message: 'select'} do
+      validates :category_id
+      validates :sales_status_id 
+      validates :prefecture_id
+      validates :shipping_fee_status_id
+      validates :scheduled_delivery_id
+    end
+  end
 end
