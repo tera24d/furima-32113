3e5a5c8f532417561ee7364b56f5df3a559b86fd
tera24d <tera12d@gmail.com>
2020-11-21 11:09:39 +0900

    バリデーションの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :info, length: { maximum: 1000 }
-    validates :price, numericality: { greater_than_or_equal_to: 300 }
-    validates :price, numericality: { less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
     with_options numericality: { other_than: 0, message: 'select' } do
       validates :category_id
