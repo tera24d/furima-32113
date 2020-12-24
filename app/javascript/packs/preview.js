7568d78628f41c8d9ec09c717449fbb5d724f4ff
tera24d <tera12d@gmail.com>
2020-12-25 00:52:50 +0900

    プレビュー機能

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
 
-  document.getElementById('item-image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+
+      blobImage.style.width = '300px'
+      blobImage.style.height = 'auto'
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
   });
-});
+}
